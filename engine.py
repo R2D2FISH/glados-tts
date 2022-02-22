acc7ad47e70d01cf34004aeeee59e0041f205ada
Henri Rantanen <nerdaxic+github@gmail.com>
2022-02-22 21:09:03 +0200

    Modified engine to better serve voice assistant
    Former-commit-id: 98ea619e7678a0cbd40bf2009e09925b75b5d79d

--- engine.py
+++ engine.py
@@ -1,15 +1,13 @@
 # importing sys
 import sys
-  
-# adding Folder_2/subfolder to the system path
-sys.path.insert(0, '/home/nerdaxic/glados-voice-assistant/glados_tts')
+import os
+
+sys.path.insert(0, os.getcwd()+'/glados_tts')
 
 import torch
 from utils.tools import prepare_text
 from scipy.io.wavfile import write
-from playsound import playsound as ps
 import time
-
 
 
 glados = torch.jit.load('glados_tts/models/glados.pt')
@@ -30,20 +28,21 @@ else:
 glados.cpu()
 vocoder.to(device)
 
-def speak(text):
+def glados_tts(text):
     x = prepare_text(text).to('cpu')
 
     with torch.no_grad():
         old_time = time.time()
         tts_output = glados.generate_jit(x)
-        print("Forward Tacotron took " + str((time.time() - old_time) * 1000) + "ms")
+        print("Forward Tacotron took " + str(round((time.time() - old_time) * 1000)) + " ms")
         old_time = time.time()
         mel = tts_output['mel_post'].cpu()
         audio = vocoder(mel)
-        print("HiFiGAN took " + str((time.time() - old_time) * 1000) + "ms")
+        print("HiFiGAN took " + str(round((time.time() - old_time) * 1000)) + " ms\n")
         audio = audio.squeeze()
         audio = audio * 32768.0
         audio = audio.cpu().numpy().astype('int16')
         output_file = ('output.wav')
         write(output_file, 22050, audio)
-        ps('output.wav')
+
+    return True
