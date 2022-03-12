c24370384ff0739f20a6709963a54dc16b4009b1
Henri Rantanen <nerdaxic+github@gmail.com>
2022-03-12 18:18:42 +0200

    GPU Optimizations for engine
    Former-commit-id: d1f233c0b901152784976991d8d9c4c4b0164606

--- engine.py
+++ engine.py
@@ -9,36 +9,38 @@ from utils.tools import prepare_text
 from scipy.io.wavfile import write
 import time
 
+sys.path.insert(0, os.getcwd()+'/glados_tts')
 
-glados = torch.jit.load('glados_tts/models/glados.pt')
+print("Initializing TTS Engine...")
 
 if torch.is_vulkan_available():
     device = 'vulkan'
-    vocoder = torch.jit.load('glados_tts/models/vocoder-gpu.pt')
-    print("Initializing TTS Engine on GPU (Vulkan)")
 if torch.cuda.is_available():
     device = 'cuda'
-    vocoder = torch.jit.load('glados_tts/models/vocoder-gpu.pt')
-    print("Initializing TTS Engine on GPU (CUDA)")
 else:
     device = 'cpu'
-    vocoder = torch.jit.load('glados_tts/models/vocoder-cpu-hq.pt')
-    print("Initializing TTS Engine on CPU")
 
-glados.cpu()
-vocoder.to(device)
+glados = torch.jit.load('glados_tts/models/glados.pt')
+vocoder = torch.jit.load('glados_tts/models/vocoder-gpu.pt', map_location=device)
+
+for i in range(4):
+    init = glados.generate_jit(prepare_text(str(i)))
+    init_mel = init['mel_post'].to(device)
+    init_vo = vocoder(init_mel)
+
 
 def glados_tts(text):
+
     x = prepare_text(text).to('cpu')
 
     with torch.no_grad():
         old_time = time.time()
         tts_output = glados.generate_jit(x)
-        print("Forward Tacotron took " + str(round((time.time() - old_time) * 1000)) + " ms")
+        print("Forward Tacotron took " + str((time.time() - old_time) * 1000) + "ms")
         old_time = time.time()
-        mel = tts_output['mel_post'].cpu()
+        mel = tts_output['mel_post'].to(device)
         audio = vocoder(mel)
-        print("HiFiGAN took " + str(round((time.time() - old_time) * 1000)) + " ms\n")
+        print("HiFiGAN took " + str((time.time() - old_time) * 1000) + "ms")
         audio = audio.squeeze()
         audio = audio * 32768.0
         audio = audio.cpu().numpy().astype('int16')
