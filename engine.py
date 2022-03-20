1ffa7d7203f9019d09538751996086cc64a24259
eternalliving <eternalliving@gmail.com>
2022-03-20 07:41:03 -0700

    Fixing all my mistakes :)

--- engine.py
+++ engine.py
@@ -11,11 +11,11 @@ print("\033[1;94mINFO:\033[;97m Initializing TTS Engine...")
 
 # Select the device
 if torch.is_vulkan_available():
-    device = 'vulkan'
+	device = 'vulkan'
 if torch.cuda.is_available():
-    device = 'cuda'
+	device = 'cuda'
 else:
-    device = 'cpu'
+	device = 'cpu'
 
 # Load models
 glados = torch.jit.load('models/glados.pt')
@@ -23,41 +23,41 @@ vocoder = torch.jit.load('models/vocoder-gpu.pt', map_location=device)
 
 # Prepare models in RAM
 for i in range(4):
-    init = glados.generate_jit(prepare_text(str(i)))
-    init_mel = init['mel_post'].to(device)
-    init_vo = vocoder(init_mel)
+	init = glados.generate_jit(prepare_text(str(i)))
+	init_mel = init['mel_post'].to(device)
+	init_vo = vocoder(init_mel)
 
 
 def glados_tts(text, key=False):
 
-    # Tokenize, clean and phonemize input text
-    x = prepare_text(text).to('cpu')
+	# Tokenize, clean and phonemize input text
+	x = prepare_text(text).to('cpu')
 
-    with torch.no_grad():
+	with torch.no_grad():
 
-        # Generate generic TTS-output
-        old_time = time.time()
-        tts_output = glados.generate_jit(x)
+		# Generate generic TTS-output
+		old_time = time.time()
+		tts_output = glados.generate_jit(x)
 
-        # Use HiFiGAN as vocoder to make output sound like GLaDOS
-        mel = tts_output['mel_post'].to(device)
-        audio = vocoder(mel)
-        print("\033[1;94mINFO:\033[;97m The audio sample took " + str(round((time.time() - old_time) * 1000)) + " ms to generate.")
+		# Use HiFiGAN as vocoder to make output sound like GLaDOS
+		mel = tts_output['mel_post'].to(device)
+		audio = vocoder(mel)
+		print("\033[1;94mINFO:\033[;97m The audio sample took " + str(round((time.time() - old_time) * 1000)) + " ms to generate.")
 
-        # Normalize audio to fit in wav-file
-        audio = audio.squeeze()
-        audio = audio * 32768.0
-        audio = audio.cpu().numpy().astype('int16')
+		# Normalize audio to fit in wav-file
+		audio = audio.squeeze()
+		audio = audio * 32768.0
+		audio = audio.cpu().numpy().astype('int16')
 		if(key):
 			output_file = ('audio/GLaDOS-tts-temp-output-'+key+'.wav')
 		else:
 			output_file = ('audio/GLaDOS-tts-temp-output.wav')
 
-        # Write audio file to disk
-        # 22,05 kHz sample rate 
-        write(output_file, 22050, audio)
+		# Write audio file to disk
+		# 22,05 kHz sample rate 
+		write(output_file, 22050, audio)
 
-    return True
+	return True
 
 
 # If the script is run directly, assume remote engine
@@ -68,10 +68,9 @@ if __name__ == "__main__":
 	CACHE = True
 
 	from flask import Flask, request, send_file
-	from utils.tools import cleanTSSFile
 	import urllib.parse
 	
-	print("Initializing TTS Server...")
+	print("\033[1;94mINFO:\033[;97m Initializing TTS Server...")
 	
 	app = Flask(__name__)
 
@@ -81,7 +80,11 @@ if __name__ == "__main__":
 		if(text == ''): return 'No input'
 		
 		line = urllib.parse.unquote(request.url[request.url.find('synthesize/')+11:])
-		file = os.getcwd()+'/audio/'+cleanTTSFile(line)
+		filename = "GLaDOS-tts-"+line.replace(" ", "-")
+		filename = filename.replace("!", "")
+		filename = filename.replace("°c", "degrees celcius")
+		filename = filename.replace(",", "")+".wav"
+		file = os.getcwd()+'/audio/'+filename
 		
 		# Check for Local Cache
 		if(os.path.isfile(file)):
@@ -92,14 +95,14 @@ if __name__ == "__main__":
 			return send_file(file)
 			
 		# Generate New Sample
-		key = time.time()[7:]
+		key = str(time.time())[7:]
 		if(glados_tts(line, key)):
 			tempfile = os.getcwd()+'/audio/GLaDOS-tts-temp-output-'+key+'.wav'
 			return send_file(tempfile)
 			
 			# If the line isn't too long, store in cache
-			if(len(line) < 200):
-				shutil.move(tempfile, 'audio/'+file)
+			if(len(line) < 200 and CACHE):
+				shutil.move(tempfile, filename)
 			else:
 				os.remove(tempfile)
 				
@@ -108,4 +111,4 @@ if __name__ == "__main__":
 			
 	cli = sys.modules['flask.cli']
 	cli.show_server_banner = lambda *x: None
-	app.run(host="0.0.0.0", PORT)
+	app.run(host="0.0.0.0", port=PORT)
